b3c7e1863d70032736a7580424f1928d57d7063e
Rafael <Fnac@Dell.(none)>
2013-10-20 21:41:31 -0200

    Planning

--- client/client.c
+++ client/client.c
@@ -1,1 +1,6 @@
-﻿
+﻿//fonte do client
+// 1 - na primeira conexão receberá uma chave pública e irá mantê-la
+// 2 - recebe texto através da entrada padrão  (scanf)
+// 3 - usa os algorimos para criptografar a mensagem
+// 4 - conecta no server e entrega a mensagem
+
