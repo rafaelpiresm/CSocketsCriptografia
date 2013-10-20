bfa8ca9a31f85dfd486c5b8154e2d3d5b119d047
Rafael <Fnac@Dell.(none)>
2013-10-20 21:39:15 -0200

    Planning

--- server/server.c
+++ server/server.c
@@ -1,1 +1,4 @@
-﻿
+﻿//fonte do server
+//1 - ficará escutando initerruptamente a conexão do client
+//2 - se receber um pedido de geração de chave pública, entregará ao client uma nova chave pública
+//3 - se receber uma mensagem criptografada, irá exibir em tela a mensagem descriptografada
