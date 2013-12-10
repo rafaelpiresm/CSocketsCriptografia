01995f7bf4ae76f4ef543262553bb93d930b5fa9
Alessandro <alessandro.almeida@tecnoprax.com>
2013-12-09 22:35:33 -0200

    Adaptação para C
    Signed-off-by: Alessandro <alessandro.almeida@tecnoprax.com>

--- client/client.c
+++ client/client.c
@@ -11,122 +11,147 @@
 #define MAXLINE 4096 /*tamanho máximo de entrada*/
 #define SERV_PORT 3000 /*porta*/
 
-char *cifra_mensagem(char *palavra, char *chave, int tamanho)
+char* 
+cifra_mensagem(char *palavra, char *chave, int tamanho)
 {
-    //alocando memória dinamicamente para a chave
-    char *palavra_cifrada = new char[tamanho];
-    int i = 0;
-    for(i = 0; i < tamanho; i++)
-    {
-        //algoritmo para a cifra damensagem
-        //C = P + K (mod 26)
-        int caractere_cifrado = int(palavra[i]) + int(chave[i]) % 26;
-        palavra_cifrada[i] = (char)caractere_cifrado;
-    }                   
-    return palavra_cifrada;
+     /* alocando memória dinamicamente para a chave */
+     char *palavra_cifrada = malloc(sizeof(char) * tamanho);
+     int i = 0;
+     for(i = 0; i < tamanho; i++)
+     {
+	  /*
+	  //algoritmo para a cifra damensagem
+	  //C = P + K (mod 26)
+	  */
+	  int caractere_cifrado = ((int) palavra[i]) + ((int) chave[i]) % 26;
+	  palavra_cifrada[i] = (char) caractere_cifrado;
+     }                   
+     
+     return palavra_cifrada;
 }
 
-void configura_start_random()
+void 
+configura_start_random()
 {
-    srand(time(NULL));
+     srand(time(NULL));
 }
 
-int gera_um_digito_chave ()
+int 
+gera_um_digito_chave ()
 {
-    int r = 1 + (rand() % 25);
-    return r;
+     int r = 1 + (rand() % 25);
+     return r;
 }
  
-char *gera_chave(char *chave, int tamanho)
+char* 
+gera_chave(char *chave, int tamanho)
 {
-        int i;
-        chave = new char[tamanho];
-        for (i = 0; i<tamanho; i++)        
-        {
-              int digito_randomico = gera_um_digito_chave();                    
-            chave[i] = (char)digito_randomico;        
-        }                        
-        return chave;
+     int i;
+     chave = malloc(sizeof(char) * tamanho);
+     for (i = 0; i<tamanho; i++)        
+     {
+	  int digito_randomico = gera_um_digito_chave();                    
+	  chave[i] = (char)digito_randomico;        
+     }                        
+     
+     return chave;
 }
 
 
-int main(int argc, char **argv)
+int 
+main(int argc, char **argv)
 {
-    int sockfd;
-    struct sockaddr_in servaddr;
-    char sendline[MAXLINE];    
-    
-    if (argc !=2) 
-    {
-        perror("Use da seguinte forma: client <IP do servidor>");
-        exit(1);
-    }
+     int sockfd;
+     struct sockaddr_in servaddr;
+     char sendline[MAXLINE];    
+     
+     if (argc !=2) 
+     {
+	  perror("Use da seguinte forma: client <IP do servidor>");
+	  exit(1);
+     }
+     /*
+     //Criando socket para o cliente    
+     */
+     if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) 
+     {
+	  perror("Problema criando o socket");
+	  exit(2);
+     }
+     
+     /*
+     //configurando o socket com IP e porta
+     */
+     memset(&servaddr, 0, sizeof(servaddr));
+     servaddr.sin_family = AF_INET;
+     servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+     servaddr.sin_port =  htons(SERV_PORT); 
+     
+     /*
+     //fazendo a conexão com o servidor
+     */
+     if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) 
+     {
+	  perror("Problema na conexão com o servidor");
+	  exit(3);
+     }
+     else
+     {
+	  printf("\nConexão estabelecida com sucesso!\n");
+	  printf("Host: %s",argv[1]);
+     }
+     
+     printf("\n\nDigite a mensagem a ser criptografada e enviada ao servidor:\n");
+     while (fgets(sendline, MAXLINE, stdin) != NULL) 
+     {
+	  char *chave;
+	  char *palavra_cifrada;
+	  int qtd_data_received;
 
-    //Criando socket para o cliente    
-    if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) 
-    {
-        perror("Problema criando o socket");
-        exit(2);
-    }
+	  /*
+	  //configura função randômica para geração automática da chave
+	  */
+	  configura_start_random();        
+	  
+	  /*
+	  //gera a chave de acordo com o tamanho da entrada
+	  */
+	  chave = gera_chave(chave,strlen(sendline));
+	  
+	  /*
+	  //cifra a mensagem informada de acordo com a chave gerada
+	  */
+	  palavra_cifrada = cifra_mensagem(sendline,chave,strlen(sendline));   
 
-    //configurando o socket com IP e porta
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr= inet_addr(argv[1]);
-    servaddr.sin_port =  htons(SERV_PORT); 
-
-    //fazendo a conexão com o servidor
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) 
-    {
-        perror("Problema na conexão com o servidor");
-        exit(3);
-    }
-    else
-    {
-    	printf("\nConexão estabelecida com sucesso!\n");
-    	printf("Host: %s",argv[1]);
-    }
-
-    printf("\n\nDigite a mensagem a ser criptografada e enviada ao servidor:\n");
-    while (fgets(sendline, MAXLINE, stdin) != NULL) 
-    {
-        //configura função randômica para geração automática da chave
-        configura_start_random();        
-
-        //gera a chave de acordo com o tamanho da entrada
-        char *chave = gera_chave(chave,strlen(sendline));
-
-        //cifra a mensagem informada de acordo com a chave gerada
-        char *palavra_cifrada = cifra_mensagem(sendline,chave,strlen(sendline));   
-        printf("A palavra cifrada é: %s\n",palavra_cifrada);
-        printf("A chave é: %s\n",chave);
-
-        //envio da mensagem cifrada
-        int qtd_data_received = send(sockfd, palavra_cifrada, MAXLINE, 0);
-
-        if (qtd_data_received > 0)
-        	printf("A palavra cifrada foi entregue!\n");
-        else
-        {
-        	printf("A mensagem não pode ser entregue!");
-        	exit(3);
-        }        
-
-        //envio chave gerada
-        qtd_data_received = send(sockfd, chave, MAXLINE, 0);
-        if (qtd_data_received > 0)
-            printf("A chave foi entregue!\n");
-        else
-        {
-            printf("A chave não pode ser entregue!\n");
-            exit(3);
-        }                           
-
-        printf("\n\nDigite a mensagem a ser criptografada e enviada ao servidor:\n");
-
-        //desalocando memória alocada dinamicamente para a chave
-        delete(chave);
-    }
-    //saída OK para o SO
-    exit(0);
+	  printf("A palavra cifrada é: %s\n",palavra_cifrada);
+	  printf("A chave é: %s\n",chave);
+	  
+	  /* envio da mensagem cifrada */
+	  qtd_data_received = send(sockfd, palavra_cifrada, MAXLINE, 0);
+	  
+	  if (qtd_data_received > 0)
+	       printf("A palavra cifrada foi entregue!\n");
+	  else
+	  {
+	       printf("A mensagem não pode ser entregue!");
+	       exit(3);
+	  }        
+	  
+	  /* envio chave gerada */
+	  qtd_data_received = send(sockfd, chave, MAXLINE, 0);
+	  if (qtd_data_received > 0)
+	       printf("A chave foi entregue!\n");
+	  else
+	  {
+	       printf("A chave não pode ser entregue!\n");
+	       exit(3);
+	  }                           
+	  
+	  printf("\n\nDigite a mensagem a ser criptografada e enviada ao servidor:\n");
+	  
+	  /* desalocando memória alocada dinamicamente para a chave */
+	  free(chave);
+     }
+     /* saída OK para o SO */
+     exit(0);
 }
--- server/server.c
+++ server/server.c
@@ -12,95 +12,117 @@
 
 void clean_entrada(char *buf)
 {
-        int i;
-        for(i = 0; i < MAXLINE; i++)
-                buf[i] = '\0';
+     int i;
+     for(i = 0; i < MAXLINE; i++)
+	  buf[i] = '\0';
 }
 
-char *decifra_mensagem(char *chave, char *palavra_cifrada, int tamanho)
+char* 
+decifra_mensagem(char *chave, char *palavra_cifrada, int tamanho)
 {  
-  //alocando memória dinamicamente para a palavra a ser decifrada
-  char *palavra_decifrada = new char[tamanho + 1];
-  //char *palavra_decifrada = (char *)malloc(sizeof(char) * tamanho +1);
-  int i = 0;
-  for(i = 0; i < tamanho; i++)
-  {
-    //algoritmo para decifrar a mensagem
-    //C = C - K + 26 (mod 26)
-    int c = int(chave[i]);
-    //int c = (int)chave[i];
-    int p = int(palavra_cifrada[i]);
-    //int p = (int)palavra_cifrada[i];
-    int caractre_decifrado = 0;
-    if (c > p)
-      caractre_decifrado = c - p  + 26 % 26; 
-    else
-      caractre_decifrado = p - c  + 26 % 26;
-      palavra_decifrada[i] = char(caractre_decifrado);
-  }       
-  return palavra_decifrada;  
+     /*
+     //alocando memória dinamicamente para a palavra a ser decifrada
+     */
+     char *palavra_decifrada = malloc(sizeof(char) * (tamanho+1));
+ 
+     int i = 0;
+
+     for(i = 0; i < tamanho; i++)
+     {
+	  /*
+	  //algoritmo para decifrar a mensagem
+	  //C = C - K + 26 (mod 26)
+	  */
+	  int c = (int) chave[i];
+	  int p = (int) palavra_cifrada[i];
+	  int caractre_decifrado = 0;
+	  if (c > p)
+	       caractre_decifrado = c - p  + 26 % 26; 
+	  else
+	       caractre_decifrado = p - c  + 26 % 26;
+
+	  palavra_decifrada[i] = (char) caractre_decifrado;
+     }       
+
+     return palavra_decifrada;  
 }
 
-int main (int argc, char **argv)
+int 
+main (int argc, char **argv)
 {
-   int listenfd, connfd, n;
-   socklen_t clilen;
+     int listenfd, connfd, n;
+     socklen_t clilen;
+     
+     struct sockaddr_in cliaddr, servaddr;
+     /*
+     //cria o socket
+     */
+     listenfd = socket (AF_INET, SOCK_STREAM, 0);
+     
+     
+     /*
+     //configura o socket com IP e porta
+     */
+     servaddr.sin_family = AF_INET;
+     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+     servaddr.sin_port = htons(SERV_PORT);
+     
+     bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+     /*
+     //faz o server escutar o máximo de MAX_CONNECTIONS
+     */
+     listen(listenfd, MAX_CONNECTIONS);
+     
+     printf("%s\n","Server online... Aceitando novas conexões...");
+     
+     clilen = sizeof(cliaddr);
+     connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+     printf("%s\n","Conexão recebida... aguardando mensagem...");
+     
+     for ( ; ; ) 
+     {   
+	  char* mensagem;
+	  char *buf = malloc(sizeof(char) * MAXLINE);
+	  char *chave = malloc(sizeof(char) * MAXLINE);
 
-   struct sockaddr_in cliaddr, servaddr;
+	  /*
+	  //recebe a mensagem
+	  */
+	  n = recv(connfd, buf, MAXLINE,0);             
+	  if (n <= 0) 
+	  {
+	       perror("Falha na leitura. Encerrando servidor.");
+	       close(connfd);
+	       close (listenfd);
+	       exit(1);
+	  }
+	  
+	  printf("%s\n","Mensagem criptografada recebida:");
+	  puts(buf);
+	  
+	  /*
+	  //recebe a chave
+	  */
+	  n = recv(connfd, chave, MAXLINE,0);          
+	  mensagem = decifra_mensagem(chave, buf, MAXLINE);
+	  
+	  printf("A mensagem decifrada é: \n%s", mensagem);           
 
-   //cria o socket
-   listenfd = socket (AF_INET, SOCK_STREAM, 0);
-
-   //configura o socket com IP e porta
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   servaddr.sin_port = htons(SERV_PORT);
-
-   bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-   
-   //faz o server escutar o máximo de MAX_CONNECTIONS
-   listen(listenfd, MAX_CONNECTIONS);
-
-   printf("%s\n","Server online... Aceitando novas conexões...");
-
-   clilen = sizeof(cliaddr);
-   connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
-   printf("%s\n","Conexão recebida... aguardando mensagem...");
-
-   for ( ; ; ) 
-   {   
-      char *buf = new char[MAXLINE];
-      char *chave = new char[MAXLINE];
-      //recebe a mensagem
-      n = recv(connfd, buf, MAXLINE,0);             
-      if (n <= 0) 
-      {
-         perror("Falha na leitura. Encerrando servidor.");
-         close(connfd);
-         close (listenfd);
-         exit(1);
-      }
-
-      printf("%s\n","Mensagem criptografada recebida:");
-      puts(buf);
-
-      //recebe a chave
-      n = recv(connfd, chave, MAXLINE,0);          
-       
-      //decifra a mensagem
-      char *mensagem = decifra_mensagem(chave,buf,MAXLINE);
-      
-      printf("A mensagem decifrada é: \n%s", mensagem);           
-
-      //limpa as variáveis de conteúdo
-      clean_entrada(buf);
-      clean_entrada(chave);
-      delete(mensagem);
-      //clean_entrada(mensagem);      
-
-      puts("================================================================");
-    }   
-   //encerra a conexão
-   close(connfd);
-   close(listenfd);
+	  /*
+	  //limpa as variáveis de conteúdo
+	  */
+	  clean_entrada(buf);
+	  clean_entrada(chave);
+	  free(mensagem);
+	  /*
+	  //clean_entrada(mensagem);      
+	  */
+	  puts("================================================================");
+     }
+     
+/*   
+//encerra a conexão
+*/
+     close(connfd);
+     close(listenfd);
 }
